adb47e1c954a08d8c9f2dac0abe89c7a8c99e426
unknown <najimuddinhelalal670@gmail.com>
2025-08-23 23:50:52 +0600

    added role in constants

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+}
+
+export interface ISidebarItem {
+  title: string;
+  items: {
+    title: string;
+    url: string;
+  }[];
+}
+
+export type TRole = "ADMIN" | "DRIVER" | "RIDER";
+
+type ZodIssue = {
+  code: string;
+  expected: string;
+  received: string;
+  path: string[];
+  message: string;
+};
+
+type ErrorSource = {
+  path: string;
+  message: string;
+};
+
+export interface IErrorResponse {
+  success: boolean;
+  message: string;
+  errorSources?: ErrorSource[];
+  err?: {
+    issues: ZodIssue[];
+    name: string;
+  };
+  stack?: string;
+}
