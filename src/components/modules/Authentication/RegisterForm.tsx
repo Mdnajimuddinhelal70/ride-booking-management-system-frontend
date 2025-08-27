ee1c02afdc5d9fb321d02bff72911d8c216d33be
unknown <najimuddinhelalal670@gmail.com>
2025-08-27 16:42:21 +0430

    set up redux and created register endpoints

--- src/components/modules/Authentication/RegisterForm.tsx
+++ src/components/modules/Authentication/RegisterForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -10,8 +11,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useRegisterMutation } from "@/redux/features/auth/auth.api";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -20,7 +23,8 @@ const formSchema = z.object({
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
-export default function RegisterForm() {
+const RegisterForm = () => {
+  const [register] = useRegisterMutation();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,9 +34,21 @@ export default function RegisterForm() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    const userInfo = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+    try {
+      const result = await register(userInfo).unwrap();
+      toast.success("User register successfully.");
+      console.log(result);
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error);
+    }
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center">
@@ -108,4 +124,6 @@ export default function RegisterForm() {
       </div>
     </div>
   );
-}
+};
+
+export default RegisterForm;
