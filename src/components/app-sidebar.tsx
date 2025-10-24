4cec7c9f54b4f1787adaa84d0fa6aa5ca6ebcab4
unknown <najimuddinhelalal670@gmail.com>
2025-10-25 00:15:07 +0600

    added log out function in sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,53 +1,3 @@
-// import * as React from "react";
-
-// import {
-//   Sidebar,
-//   SidebarContent,
-//   SidebarGroup,
-//   SidebarGroupContent,
-//   SidebarGroupLabel,
-//   SidebarHeader,
-//   SidebarMenu,
-//   SidebarMenuButton,
-//   SidebarMenuItem,
-//   SidebarRail,
-// } from "@/components/ui/sidebar";
-// import { getSidebarItems } from "@/utils/getSidebarItems";
-// import { Link } from "react-router";
-
-// // This is sample data.
-// const data = {
-//   navMain: getSidebarItems("RIDER"),
-// };
-
-// export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-//   return (
-//     <Sidebar {...props}>
-//       <SidebarHeader>{/* {Logo} */}</SidebarHeader>
-//       <SidebarContent>
-//         {/* We create a SidebarGroup for each parent. */}
-//         {data.navMain.map((item) => (
-//           <SidebarGroup key={item.title}>
-//             <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-//             <SidebarGroupContent>
-//               <SidebarMenu>
-//                 {item.items.map((item) => (
-//                   <SidebarMenuItem key={item.title}>
-//                     <SidebarMenuButton asChild>
-//                       <Link to={item.url}>{item.title}</Link>
-//                     </SidebarMenuButton>
-//                   </SidebarMenuItem>
-//                 ))}
-//               </SidebarMenu>
-//             </SidebarGroupContent>
-//           </SidebarGroup>
-//         ))}
-//       </SidebarContent>
-//       <SidebarRail />
-//     </Sidebar>
-//   );
-// }
-
 import {
   Sidebar,
   SidebarContent,
@@ -60,18 +10,27 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
-import { useAuth } from "@/context/AuthContext"; // ✅ import করো
+import { useAuth } from "@/context/AuthContext";
+import { authApi, useLogoutMutation } from "@/redux/features/auth/auth.api";
 import { getSidebarItems } from "@/utils/getSidebarItems";
 import * as React from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [logout] = useLogoutMutation();
+  const dispatch = useDispatch();
   const { user } = useAuth();
   const role = user?.role || "RIDER";
   const data = {
     navMain: getSidebarItems(role),
   };
 
+  const handleLogout = async () => {
+    await logout(undefined);
+    dispatch(authApi.util.resetApiState());
+    window.location.href = "/login";
+  };
   return (
     <Sidebar {...props}>
       <SidebarHeader>{/* {Logo} */}</SidebarHeader>
@@ -94,7 +53,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         ))}
       </SidebarContent>
       <SidebarRail />
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </Sidebar>
   );
 }
