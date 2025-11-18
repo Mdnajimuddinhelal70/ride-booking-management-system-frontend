e0d42cfe90b548e70be46a86d3fd6e9babcdc2eb
unknown <najimuddinhelalal670@gmail.com>
2025-11-18 14:55:04 +0600

    added swiper pagination in home page's banner

--- src/components/modules/Homepage/HeroBanner.tsx
+++ src/components/modules/Homepage/HeroBanner.tsx
@@ -1,31 +1,61 @@
-import { Button } from "@/components/ui/button";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Autoplay, Pagination } from "swiper/modules";
+
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import img1 from "../../../assets/bannerImgs/bike1.jpg";
+import img2 from "../../../assets/bannerImgs/bike2.jpg";
+import img3 from "../../../assets/bannerImgs/bike3.jpg";
+import img4 from "../../../assets/bannerImgs/bike4.jpg";
 
 const HeroBanner = () => {
   return (
-    <section
-      className="relative bg-gradient-to-r
-     from-indigo-500 via-purple-500 to-pink-500 text-white py-24"
-    >
-      <div className="container mx-auto text-center px-4">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">
-          Welcome to Our Platform 🚀
-        </h1>
-        <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
-          Book rides, manage your account, and explore new features with ease.
-        </p>
-        <div className="flex justify-center gap-4">
-          <Button
-            size="lg"
-            className="bg-white text-indigo-600 hover:bg-gray-200"
-          >
-            Get Started
-          </Button>
-          <Button size="lg" className="border-white hover:bg-white/20">
-            Learn More
-          </Button>
-        </div>
-      </div>
-    </section>
+    <div className="w-full mx-auto px-4 py-6">
+      <Swiper
+        spaceBetween={30}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="rounded-xl overflow-hidden shadow-lg"
+      >
+        <SwiperSlide>
+          <img
+            src={img1}
+            alt=""
+            className="w-full h-[320px] md:h-[550px] object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={img2}
+            alt=""
+            className="w-full h-[320px] md:h-[450px] object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={img3}
+            alt=""
+            className="w-full h-[320px] md:h-[450px] object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={img4}
+            alt=""
+            className="w-full h-[320px] md:h-[450px] object-cover"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
